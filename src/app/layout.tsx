35671d5c808fe77eef6152248c7b7099e613bfc6
brianchege2k <chegebrian84@gmail.com>
2026-02-09 15:38:13 +0300

    vercel speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { getBaseURL } from "@lib/util/env"
 import type { Metadata } from "next"
 import { DM_Sans } from "next/font/google"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import "styles/globals.css"
 
 const dmSans = DM_Sans({
