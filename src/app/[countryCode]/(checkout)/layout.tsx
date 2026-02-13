a1c50e86b14723ab28dffb236abfdda606373c5a
brianchege2k <chegebrian84@gmail.com>
2026-02-13 16:44:51 +0300

    checkout layout change

--- src/app/[countryCode]/(checkout)/layout.tsx
+++ src/app/[countryCode]/(checkout)/layout.tsx
@@ -1,6 +1,6 @@
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import ChevronDown from "@modules/common/icons/chevron-down"
 import Footer from "@modules/layout/templates/footer"
+import Nav from "@modules/layout/templates/nav" // <-- use your real nav component path
+
 export default function CheckoutLayout({
   children,
 }: {
@@ -8,32 +8,13 @@ export default function CheckoutLayout({
 }) {
   return (
     <div className="w-full bg-white relative small:min-h-screen">
-      <div className="h-16 bg-white border-b ">
-        <nav className="flex h-full items-center content-container justify-between">
-          <LocalizedClientLink
-            href="/cart"
-            className="text-small-semi text-ui-fg-base flex items-center gap-x-2 uppercase flex-1 basis-0"
-            data-testid="back-to-cart-link"
-          >
-            <ChevronDown className="rotate-90" size={16} />
-            <span className="mt-px hidden small:block txt-compact-plus text-ui-fg-subtle hover:text-ui-fg-base ">
-              Back to shopping cart
-            </span>
-            <span className="mt-px block small:hidden txt-compact-plus text-ui-fg-subtle hover:text-ui-fg-base">
-              Back
-            </span>
-          </LocalizedClientLink>
-          <LocalizedClientLink
-            href="/"
-            className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
-            data-testid="store-link"
-          >
-            Medusa Store
-          </LocalizedClientLink>
-          <div className="flex-1 basis-0" />
-        </nav>
+      {/* Use site nav */}
+      <Nav />
+
+      <div className="relative" data-testid="checkout-container">
+        {children}
       </div>
-      <div className="relative" data-testid="checkout-container">{children}</div>
+
       <div className="py-4 w-full flex items-center justify-center">
         <Footer />
       </div>
